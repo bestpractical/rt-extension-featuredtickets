6cfc66bc56a57fe5d4bf4c4f4f03ea2e24733a4f
Jim Brandt <jbrandt@bestpractical.com>
2018-07-02 13:17:26 -0400

    Verison 0.03

--- lib/RT/Extension/FeaturedTickets.pm
+++ lib/RT/Extension/FeaturedTickets.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package RT::Extension::FeaturedTickets;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 RT->AddStyleSheets("featured-tickets.css");
 RT->AddJavaScript("featured-tickets.js");
