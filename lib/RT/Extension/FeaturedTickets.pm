71373694a2bc538566aac2605179b80b725b251c
sunnavy <sunnavy@bestpractical.com>
2018-07-16 21:58:16 +0800

    Version 0.04

--- lib/RT/Extension/FeaturedTickets.pm
+++ lib/RT/Extension/FeaturedTickets.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package RT::Extension::FeaturedTickets;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 RT->AddStyleSheets("featured-tickets.css");
 RT->AddJavaScript("featured-tickets.js");
